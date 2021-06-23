92c8a667dd96383a1d89f713617931aeae64a791
Anton <kolesov.anton.s@gmail.com>
2021-06-23 18:45:18 +0300

    implement suggest-add-video.dialog

--- src/app/features/stream/components/suggest-add-video/suggest-add-video.dialog.ts
+++ src/app/features/stream/components/suggest-add-video/suggest-add-video.dialog.ts
@@ -1,4 +1,13 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, QueryList, ViewChildren} from '@angular/core';
+import {VideoCardComponent} from '../../../video/components/video-card/video-card.component';
+import {AuthService} from '../../../core/services/auth.service';
+import {StreamService} from '../../services/stream.service';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {UserService} from '../../../user/services/user.service';
+import {Video} from '../../../video/models/video.model';
+import {UserVideoService} from '../../../user/services/user-video.service';
+import {StreamVoteService} from '../../services/stream-vote.service';
+import {NewVoteAdd} from '../../models/new-vote-add.model';
 
 @Component({
   selector: 'app-suggest-add-video',
@@ -7,10 +16,47 @@ import {Component, OnInit} from '@angular/core';
 })
 export class SuggestAddVideoDialog implements OnInit {
 
-  constructor() {
+  @ViewChildren(VideoCardComponent) videoCardsRefs: QueryList<VideoCardComponent>;
+
+  // streamTitleForm = new FormControl('', [Validators.required]);
+
+  videos: Video[];
+
+  currentUserId: number;
+
+  selectedVideoCard: VideoCardComponent;
+  private streamId: number;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+    private readonly userVideoService: UserVideoService,
+    private readonly streamService: StreamService,
+    private readonly streamVoteService: StreamVoteService,
+    private readonly dialogRef: MatDialogRef<SuggestAddVideoDialog>,
+  ) {
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    this.streamId = this.data.streamId;
+    this.currentUserId = this.authService.userId;
+    this.videos = await this.userVideoService.getAllVideos(this.currentUserId).toPromise();
   }
 
+  select(videoElement: VideoCardComponent): void {
+    this.selectedVideoCard?.toggle();
+    videoElement.toggle();
+    this.selectedVideoCard = videoElement;
+  }
+
+  buttonEnabled(): boolean {
+    return this.selectedVideoCard != null;
+  }
+
+  async addStream(): Promise<void> {
+    const newVoteAdd: NewVoteAdd = {idAddVideo: this.selectedVideoCard.videoId, numberPrevVideo: 0};
+    await this.streamVoteService.addVoteAdd(this.streamId, newVoteAdd).toPromise();
+    this.dialogRef.close();
+  }
 }
