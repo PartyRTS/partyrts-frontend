d84eacbe6da880e0878323557c45079b17a50647
Anton <kolesov.anton.s@gmail.com>
2021-06-24 09:23:48 +0300

    fix server

--- server.js
+++ server.js
@@ -9,23 +9,10 @@
 const express = require('express');
 const app = express();
 
-const forceSSL = function () {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-
-app.use(forceSSL());
-
 app.use(express.static('./dist/pws-frontend'));
 
 app.get('/*', (req, res) =>
   res.sendFile('index.html', {root: 'dist/pws-frontend/'}),
 );
 
-app.listen(process.env.PORT || 8080);
+app.listen(80, '0.0.0.0');
